d27f4590931cd086b8c9568671f1e68c55149b1f
jagratikanthali28 <159175028+jagratikanthali28@users.noreply.github.com>
2024-02-06 19:39:58 +0000

    datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 144556
+let accountEmail = "jagrati.kanthali686@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+let accountState;
+//accountId = 2    not allowed const only declare once in a program
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3)    // we are using nodejs not browser
+console.log(3 
+    + 
+    3)  // ignore this kinds of things (code readability shold be high)
+console.log("jagrati")
+
+let name="jagrati"
+let age=18
+let isLoggedIn = false
+let state=null
+console.table([name, age, state])
+//number => 2 to power 53
+//bigint
+//string =>""
+//boolean => true/false
+// null => standalone value
+// undefined =>
+//symbols => unique
+// object
+                                      //output!
+console.log(typeof "hitesh");        //string
+console.log(typeof null);          //null
+console.log(typeof undefined);     // undefined
